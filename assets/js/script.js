ff97a347f18775558be4a61cc63665de3406e70a
amour05 <amoursedjro47@gmail.com>
2026-02-22 14:03:21 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".card");
+  cards.forEach(card => {
+    card.addEventListener("mouseover", () => {
+      card.classList.add("animate__pulse");
+    });
+    card.addEventListener("mouseout", () => {
+      card.classList.remove("animate__pulse");
+    });
+  });
+});
